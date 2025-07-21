fe35608cfc89bce3585c7620f0b8192ce7f85d65
mitzyvalenciaordonez <mitzyvo@gmail.com>
2025-07-21 09:23:25 -0500

    :hammer: Changed name bucket

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "Cloud_function_bucket" {
-  name      = "github-action-with-terraform-${var.project_id}"
+  name      = "github-action-with-terraform"
   location  = var.region
   project   = var.project_id
   force_destroy = true
