41850e065320820d16e5b89abc5082482da32c11
mitzyvalenciaordonez <mitzyvo@gmail.com>
2025-07-24 13:56:26 -0500

    :bug: testing

--- main.tf
+++ main.tf
@@ -9,15 +9,3 @@ resource "google_storage_bucket" "Cloud_function_bucket" {
     prevent_destroy = true
   }
 }
-
-resource "google_storage_bucket" "Cloud_function_bucket" {
-  name                        = "github-action-with-terraform-non-existing"
-  location                    = var.region
-  project                     = var.project_id
-  force_destroy               = true
-  uniform_bucket_level_access = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
